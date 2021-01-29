e2a428c344b4cf6c60e7d96562fb28802cde2d50
fujishig2 <fujishig@ualberta.ca>
2021-01-29 11:19:25 -0700

    Added comments

--- server.py
+++ server.py
@@ -30,9 +30,14 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        #get the data and split the data received by spaces
         self.data = self.request.recv(1024).strip()
         msg = self.data.decode("utf-8").split(' ')
+
+        #check if the request is a get method
         if (msg[0] == 'GET'):
+
+            #check the path and display the correct html webpage with styling added based off the path
             if (msg[1] == '/deep/' or msg[1] == '/hardcode/deep/' or msg[1] == '/hardcode/'):
                 html_file = open('./www' + msg[1] + 'index.html', 'r')
                 css = open('./www' + msg[1] + 'deep.css', 'r')
@@ -63,27 +68,37 @@ class MyWebServer(socketserver.BaseRequestHandler):
                 self.request.sendall(str.encode("<style>\n", 'utf-8'))
                 self.request.sendall(str.encode(css.read(), 'utf-8'))
                 self.request.sendall(str.encode("</style>\n", 'utf-8'))
-            elif(msg[1] == '/deep' or msg[1] == '/hardcode' or msg[1] == '/hardcode/deep'):
-                self.request.sendall(str.encode("HTTP/1.1 301 Moved Permanently\n", 'utf-8'))
-                self.request.sendall(str.encode("Content-Type: text/html\n", 'utf-8'))
-                self.request.send(str.encode('\r\n'))
-                self.request.sendall(str.encode('<html><head></head><body><h1>301 Moved Permanently</h1><p>Location: http://localhost:8080' + msg[1] + '/</p></body></html>', 'utf-8'))
+            
+            #return the specified css file
             elif(msg[1] == '/base.css' or msg[1] == '/deep/deep.css' or msg[1] == '/hardcode/deep.css' or msg[1] == '/hardcode/deep/deep.css'):
                 css = open('./www' + msg[1], 'r')
                 self.request.sendall(str.encode("HTTP/1.1 200 OK\n", 'utf-8'))
                 self.request.sendall(str.encode("Content-Type: text/css\n", 'utf-8'))
                 self.request.send(str.encode('\r\n'))
                 self.request.sendall(str.encode(css.read(), 'utf-8'))
+
+            #check for 301 errors with missing slashes
+            elif(msg[1] == '/deep' or msg[1] == '/hardcode' or msg[1] == '/hardcode/deep'):
+                self.request.sendall(str.encode("HTTP/1.1 301 Moved Permanently\n", 'utf-8'))
+                self.request.sendall(str.encode("Content-Type: text/html\n", 'utf-8'))
+                self.request.send(str.encode('\r\n'))
+                self.request.sendall(str.encode('<html><head></head><body><h1>301 Moved Permanently</h1><p>Location: http://localhost:8080' + msg[1] + '/</p></body></html>', 'utf-8'))
+            
+            #display 404 if webpage requested is not found
             else:
                 self.request.sendall(str.encode("HTTP/1.1 404 Not Found\n", 'utf-8'))
                 self.request.sendall(str.encode("Content-Type: text/html\n", 'utf-8'))
                 self.request.send(str.encode('\r\n'))
                 self.request.sendall(str.encode('<html><head></head><body><h1>404 Not Found</h1></body></html>', 'utf-8'))
+        
+        #display 405 if the method is anything but a get request.
         else:
             self.request.sendall(str.encode("HTTP/1.1 405 Method Not Allowed\n", 'utf-8'))
             self.request.sendall(str.encode("Content-Type: text/html\n", 'utf-8'))
             self.request.send(str.encode('\r\n'))
             self.request.sendall(str.encode('<html><head></head><body><h1>405 Method Not Allowed</h1></body></html>', 'utf-8'))
+
+        #close the socket request
         self.request.close()
 
 if __name__ == "__main__":
